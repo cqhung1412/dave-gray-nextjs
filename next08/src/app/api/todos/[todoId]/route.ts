7bd45d10efe3b8a41da0fbee003761e597a7fca4
hungcq3 <hungcq3@vng.com.vn>
2023-06-13 20:12:25 +0700

    full rest api

--- next08/src/app/api/todos/[todoId]/route.ts
+++ next08/src/app/api/todos/[todoId]/route.ts
@@ -1,13 +1,75 @@
 import { NextResponse, NextRequest } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
+const API_KEY: string = process.env.DATA_API_KEY as string;
+
+export async function GET(request: NextRequest) {
+  const id = request.nextUrl.pathname.split("/").pop() as string;
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    headers: {
+      "API-Key": API_KEY,
+    },
+  });
+
+  const todo: Todo = await res.json();
+  if (!todo.id)
+    return NextResponse.json(
+      {
+        message: "Todo not found",
+      },
+      { status: 404 }
+    );
+  return NextResponse.json(todo);
+}
+
+export async function PUT(request: NextRequest) {
+  const id = request.nextUrl.pathname.split("/").pop() as string;
+  const { title, userId, completed }: Partial<Todo> = await request.json();
+  if (!id || !title || !userId || typeof completed !== "boolean")
+    return NextResponse.json(
+      {
+        message: "Missing info",
+      },
+      { status: 400 }
+    );
+
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+    body: JSON.stringify({
+      title,
+      userId,
+      completed,
+    }),
+  });
+
+  const updatedTodo = (await res.json()) as Todo;
+
+  return NextResponse.json({
+    message: `Todo ${updatedTodo.id} updated`,
+    todo: updatedTodo,
+  });
+}
 
 export async function DELETE(request: NextRequest) {
-  const id = request.nextUrl.pathname.split("/").pop()
-  console.log("id: ", id)
-  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, { method: "DELETE" });
+  const id = request.nextUrl.pathname.split("/").pop() as string;
+  if (Number.isNaN(Number(id)))
+    return NextResponse.json(
+      {
+        message: "Invalid ID",
+      },
+      { status: 400 }
+    );
 
-  const response = await res.json();
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "API-Key": API_KEY,
+    },
+  });
 
-  return NextResponse.json(response);
+  return NextResponse.json({ message: `Todo ${id} deleted`, code: res.status });
 }
--- next08/src/app/api/todos/route.ts
+++ next08/src/app/api/todos/route.ts
@@ -1,11 +1,45 @@
 import { NextResponse } from "next/server";
 
-const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
+const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
+const API_KEY: string = process.env.DATA_API_KEY as string;
 
 export async function GET() {
-  const res = await fetch(DATA_SOURCE_URL)
+  const res = await fetch(DATA_SOURCE_URL);
 
-  const todos: Todo[] = await res.json()
+  const todos: Todo[] = await res.json();
 
-  return NextResponse.json(todos)
+  return NextResponse.json(todos);
+}
+
+export async function POST(request: Request) {
+  const { title, userId }: Partial<Todo> = await request.json();
+  if (!title || !userId) {
+    return NextResponse.json(
+      {
+        message: "Missing info",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  const res = await fetch(DATA_SOURCE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+    body: JSON.stringify({
+      title,
+      userId,
+      completed: false,
+    }),
+  });
+
+  const createdTodo = (await res.json()) as Todo;
+
+  return NextResponse.json({
+    message: `Todo ${createdTodo.id} created`,
+    todo: createdTodo,
+  });
 }
